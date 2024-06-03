c3577b93d2dbf63918ef7bff3c494a4e5e03b819
Kimera Royal <kai.anderson.ellis.brown@gmail.com>
2024-06-03 14:30:06 +0100

    v0.0.9: Unlocked mouse in title screen.

--- Assets/Games/IP1/Scripts/Interaction/LockMouse.cs
+++ Assets/Games/IP1/Scripts/Interaction/LockMouse.cs
@@ -4,10 +4,13 @@ namespace IP1.Interaction
 {
     public class LockMouse : MonoBehaviour
     {
+        [SerializeField] private CursorLockMode m_cursorLockState = CursorLockMode.Confined;
+        [SerializeField] private bool m_cursorVisible;
+        
         private void Start()
         {
-            Cursor.lockState = CursorLockMode.Confined;
-            Cursor.visible = false;
+            Cursor.lockState = m_cursorLockState;
+            Cursor.visible = m_cursorVisible;
         }
     }
 }
