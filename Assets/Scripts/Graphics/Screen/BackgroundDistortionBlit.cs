a1c52403802dcb7f5e8cfc30c2508b3af8b3317f
Kimera Royal <kai.anderson.ellis.brown@gmail.com>
2024-06-08 03:29:54 +0100

    v0.1.18: Dynamic interleave based on screen height.

--- Assets/Scripts/Graphics/Screen/BackgroundDistortionBlit.cs
+++ Assets/Scripts/Graphics/Screen/BackgroundDistortionBlit.cs
@@ -1,14 +1,39 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Anthology
 {
     public class BackgroundDistortionBlit : SimpleBlit
     {
-        [SerializeField] private TextureWrapMode m_wrapMode = TextureWrapMode.Repeat;
+        private static readonly int s_interlaceTexSt = Shader.PropertyToID("_InterlaceTex_ST");
         
+        [SerializeField] private TextureWrapMode m_wrapMode = TextureWrapMode.Repeat;
+
+        [MinValue(2)] [SerializeField] private int m_interlaceDivision = 4;
+        private int m_lastInterlaceDivision = -1;
+        private int m_lastScreenHeight = -1;
+
         protected override void ApplySourceTextureProperties(RenderTexture _source)
         {
             _source.wrapMode = m_wrapMode;
+        }
+
+        protected override void ApplyScreenEffectProperties(Material _screenEffect)
+        {
+            ApplyInterlaceDivision(_screenEffect);
+        }
+
+        private void ApplyInterlaceDivision(Material _screenEffect)
+        {
+            var screenHeight = Screen.height;
+            if(m_interlaceDivision == m_lastInterlaceDivision && screenHeight == m_lastScreenHeight) { return; }
+
+            var tilingOffset = _screenEffect.GetVector(s_interlaceTexSt);
+            tilingOffset.y = screenHeight / m_interlaceDivision;
+            _screenEffect.SetVector(s_interlaceTexSt, tilingOffset);
+            
+            m_lastInterlaceDivision = m_interlaceDivision;
+            m_lastScreenHeight = screenHeight;
         }
     }
 }
